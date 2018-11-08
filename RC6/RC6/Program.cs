e0e0199cc7d7036f2b84887bbfba9b394abbc8b9
Nikitin Dmitrii <31371098+hellondn@users.noreply.github.com>
2018-11-08 16:09:35 +0700

    aye
    oldi na meste

--- RC6/RC6/Program.cs
+++ RC6/RC6/Program.cs
@@ -6,7 +6,7 @@ namespace RC6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi Haters!");
         }
     }
 }
