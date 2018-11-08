fe95192874bf01aa236a3bd2339088060ef53c5a
Павел Петров <pashka499@yandex.ru>
2018-11-09 01:01:29 +0700

    pomoika

--- RC6/RC6/Program.cs
+++ RC6/RC6/Program.cs
@@ -8,7 +8,12 @@ namespace RC6
 {
     class Program
     {
-        private static byte[] Key;
+        private const int R=20;
+        private static UInt32[] Keys;
+        private const int W = 32;
+        private static byte[] MainKey;
+        private const UInt32 P32 = 0xB7E15163;
+        private const UInt32 Q32 = 0x9E3779B9;
         private static void GenerateKey(int Long)
         {
             AesCryptoServiceProvider aesCrypto = new AesCryptoServiceProvider // ключи самому генерировать не очень
@@ -16,9 +21,9 @@ namespace RC6
                 KeySize = Long
             };
             aesCrypto.GenerateKey();
-            Key = aesCrypto.Key;
+            MainKey = aesCrypto.Key;         
             string topkek = Encoding.Default.GetString(aesCrypto.Key);
-            Console.WriteLine(topkek); // все равно помойку выводит
+            Console.WriteLine(topkek); // просто посмотреть
         }
         private static byte[] EncodeRc6(string plaintext)
         {
@@ -35,7 +40,7 @@ namespace RC6
         private static void Test()
         {
             byte[] test = {0x8f, 0xc3, 0xa5, 0x36, 0x56, 0xb1, 0xf7, 0x78, 0xc1, 0x29, 0xdf, 0x4e, 0x98, 0x48};
-            Key = new byte[] {00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00};
+            MainKey = new byte[] {00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00};
             byte[] plaintText = { 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00 };
             string plaintTextstring = Encoding.UTF8.GetString(plaintText);
             byte[] cipher = EncodeRc6(plaintTextstring);
@@ -45,24 +50,27 @@ namespace RC6
         }
         private static void SelectKeySize()
         {
-            Console.WriteLine("Write key-size (128,192.256)");
-            short Long = Int16.Parse(Console.ReadLine());
+            Console.WriteLine("Write key-size (128,192,256)");
+            int Long = int.Parse(Console.ReadLine());
             if (Long == 1)
             {
                 Test();
             }
-            if (Long !=128 || Long!=192 || Long !=256)
+            if (Long ==128 || Long==192 || Long ==256)
+            {
+                try
+                {
+                    GenerateKey(Long);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    SelectKeySize();
+                }
+            }
+            else
             {
                 Console.WriteLine("Don't be autist");
-                SelectKeySize();
-            }
-            try
-            {
-                GenerateKey(Long);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
                 SelectKeySize();
             }
         }
