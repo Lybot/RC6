6b1e2c3bd740b58db968a7a49cc86f5645903627
Павел Петров <pashka499@yandex.ru>
2018-11-08 19:21:52 +0700

    Generate key and test logic

--- RC6/RC6/Program.cs
+++ RC6/RC6/Program.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace RC6
 {
     class Program
     {
-        private byte[] Key;
+        private static byte[] Key;
         private static void GenerateKey(int Long)
         {
-
+            AesCryptoServiceProvider aesCrypto = new AesCryptoServiceProvider // ключи самому генерировать не очень
+            {
+                KeySize = Long
+            };
+            aesCrypto.GenerateKey();
+            Key = aesCrypto.Key;
+            string topkek = Encoding.Default.GetString(aesCrypto.Key);
+            Console.WriteLine(topkek); // все равно помойку выводит
         }
         private static byte[] EncodeRc6(string plaintext)
         {
@@ -23,16 +32,30 @@ namespace RC6
             return kek;
         }
 
-        private string Test()
+        private static void Test()
         {
-            return "plaintext";
+            byte[] test = {0x8f, 0xc3, 0xa5, 0x36, 0x56, 0xb1, 0xf7, 0x78, 0xc1, 0x29, 0xdf, 0x4e, 0x98, 0x48};
+            Key = new byte[] {00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00};
+            byte[] plaintText = { 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00 };
+            string plaintTextstring = Encoding.UTF8.GetString(plaintText);
+            byte[] cipher = EncodeRc6(plaintTextstring);
+            if (test==cipher)
+                Console.WriteLine("gg easy");
+            
         }
-        static void Main(string[] args)
+        private static void SelectKeySize()
         {
-            string plainText;
-            byte[] cipherText;
-            Console.WriteLine("Write key-size (0-255)");
+            Console.WriteLine("Write key-size (128,192.256)");
             short Long = Int16.Parse(Console.ReadLine());
+            if (Long == 1)
+            {
+                Test();
+            }
+            if (Long !=128 || Long!=192 || Long !=256)
+            {
+                Console.WriteLine("Don't be autist");
+                SelectKeySize();
+            }
             try
             {
                 GenerateKey(Long);
@@ -40,9 +63,14 @@ namespace RC6
             catch (Exception e)
             {
                 Console.WriteLine(e);
-                throw;
+                SelectKeySize();
             }
-            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            string plainText;
+            byte[] cipherText;
+            SelectKeySize();
             Console.WriteLine("Write plain text");
             plainText = Console.ReadLine();
             try
