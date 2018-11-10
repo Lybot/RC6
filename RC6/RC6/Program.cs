92b8104c34e700f41a7ca71060c2f8092d4dc16e
Павел Петров <pashka499@yandex.ru>
2018-11-10 18:02:38 +0700

    milloin testov 0 rabotaet
    gg

--- RC6/RC6/Program.cs
+++ RC6/RC6/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.ComponentModel;
-using System.ComponentModel.Design;
-using System.Globalization;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -70,12 +67,56 @@ namespace RC6 // W=32  R= 20 B = 128,192,256
             for (int s = 1; s <= V; s++)
             {
                 A = RoundKey[i] = LeftShift((RoundKey[i] + A + B),ShiftCount(3));
-                B = L[j] = LeftShift(L[j] + A + B,ShiftCount((int)(A+B)));
+                B = L[j] = LeftShift((L[j] + A + B),ShiftCount((int)(A+B)));
                 i = (i + 1) % (2*R+4);
                 j = (j + 1) % c;
             }
         }
-
+        private static void GenerateKeyTest(int Long, byte[] keyCheck)
+        {
+            if (keyCheck == null)
+            {
+                AesCryptoServiceProvider aesCrypto = new AesCryptoServiceProvider // ключи самому генерировать не очень
+                {
+                    KeySize = Long
+                };
+                aesCrypto.GenerateKey();
+                MainKey = aesCrypto.Key;
+            }
+            int c = 0;
+            int i, j;
+            switch (Long)
+            {
+                case 128:
+                    c = 4;
+                    break;
+                case 192:
+                    c = 6;
+                    break;
+                case 256:
+                    c = 8;
+                    break;
+            }
+            uint[] L = new uint[c];
+            for (i = 0; i < c; i++)
+            {
+                L[i] = BitConverter.ToUInt32(MainKey, i * 4);
+            }
+            RoundKey[0] = P32;
+            for (i = 1; i < 2 * R + 4; i++)
+                RoundKey[i] = RoundKey[i - 1] + Q32;
+            uint A, B;
+            A = B = 0;
+            i = j = 0;
+            int V = Math.Max(c, 2 * R + 4); // useless
+            for (int s = 1; s <= V; s++)
+            {
+                A = RoundKey[i] = (RoundKey[i] + A + B)<< 3;
+                B = L[j] = (L[j] + A + B)<< (int) (A + B);
+                i = (i + 1) % (2 * R + 4);
+                j = (j + 1) % c;
+            }
+        }
         private static byte[] ToArrayBytes(uint[] uints,int Long)
         {
             byte[] arrayBytes = new byte[Long*4];
@@ -151,7 +192,79 @@ namespace RC6 // W=32  R= 20 B = 128,192,256
                     C = RightShift((C - RoundKey[2 * j + 1]), ShiftCount((int) t)) ^ u;
                     A = RightShift((A - RoundKey[2 * j]), ShiftCount((int) u)) ^ t;
                 }
+                D = D - RoundKey[1];
+                B = B - RoundKey[0];
+                uint[] tempWords = new uint[4] { A, B, C, D };
+                byte[] block = ToArrayBytes(tempWords, 4);
+                block.CopyTo(plainText, i);
+            }
+            return plainText;
+        }
+        private static byte[] EncodeRc6Test(string plaintext)
+        {
+            uint A, B, C, D;
+            byte[] byteText = Encoding.ASCII.GetBytes(plaintext);
+            Console.WriteLine(byteText.Length);
+            int i = byteText.Length;    //
+            while (i % 16 != 0)         //
+                i++;                    //
+            byte[] text = new byte[i]; // 
+            byteText.CopyTo(text, 0);    // мб можно проще и я аут, но это первое простое что пришло в голову, чтобы добавить 0 байты до размер блока 128 бит
+            byte[] cipherText = new byte[i];
+            for (i = 0; i < text.Length; i = i + 16)
+            {
+                A = BitConverter.ToUInt32(text, i);
+                B = BitConverter.ToUInt32(text, i + 4);
+                C = BitConverter.ToUInt32(text, i + 8);
+                D = BitConverter.ToUInt32(text, i + 12);
+                B = B + RoundKey[0];
+                D = D + RoundKey[1];
+                for (int j = 1; j <= R; j++)
+                {
+                    uint t = (B * (2 * B + 1)<< (int)(Math.Log(W, 2)));
+                    uint u = (D * (2 * D + 1)<< (int)(Math.Log(W, 2)));
+                    A = ((A ^ t)<< (int)u) + RoundKey[j * 2];
+                    C = ((C ^ u)<< (int)t) + RoundKey[j * 2 + 1];
+                    uint temp = A;
+                    A = B;
+                    B = C;
+                    C = D;
+                    D = temp;
+                }
 
+                A = A + RoundKey[2 * R + 2];
+                C = C + RoundKey[2 * R + 3];
+                uint[] tempWords = new uint[4] { A, B, C, D };
+                byte[] block = ToArrayBytes(tempWords, 4);
+                block.CopyTo(cipherText, i);
+            }
+            return cipherText;
+        }
+        private static byte[] DecodeRc6Test(byte[] cipherText)
+        {
+            uint A, B, C, D;
+            int i;
+            byte[] plainText = new byte[cipherText.Length];
+            for (i = 0; i < cipherText.Length; i = i + 16)
+            {
+                A = BitConverter.ToUInt32(cipherText, i);
+                B = BitConverter.ToUInt32(cipherText, i + 4);
+                C = BitConverter.ToUInt32(cipherText, i + 8);
+                D = BitConverter.ToUInt32(cipherText, i + 12);
+                C = C - RoundKey[2 * R + 3];
+                A = A - RoundKey[2 * R + 2];
+                for (int j = R; j >= 1; j--)
+                {
+                    uint temp = D;
+                    D = C;
+                    C = B;
+                    B = A;
+                    A = temp;
+                    uint u = (D * (2 * D + 1)<< (int)Math.Log(W, 2));
+                    uint t = (B * (2 * B + 1)<< (int)Math.Log(W, 2));
+                    C = ((C - RoundKey[2 * j + 1])>> (int)t) ^ u;
+                    A = ((A - RoundKey[2 * j])>> (int)u) ^ t;
+                }
                 D = D - RoundKey[1];
                 B = B - RoundKey[0];
                 uint[] tempWords = new uint[4] { A, B, C, D };
@@ -165,10 +278,25 @@ namespace RC6 // W=32  R= 20 B = 128,192,256
             byte[] test = {0x8f, 0xc3, 0xa5, 0x36, 0x56, 0xb1, 0xf7, 0x78, 0xc1, 0x29, 0xdf, 0x4e, 0x98, 0x48,0xa4,0x1e};
             MainKey = new byte[] {00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00};
             byte[] plaintText = {00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00};
-            string plaintTextstring = Encoding.UTF8.GetString(plaintText);
-            GenerateKey(128,MainKey);
-            byte[] cipher = EncodeRc6(plaintTextstring);
+            string plaintTextstring = Encoding.ASCII.GetString(plaintText);
+            GenerateKeyTest(128,MainKey);
+            byte[] cipher = EncodeRc6Test(plaintTextstring);
             if (test==cipher)
+                Console.WriteLine("gg easy");
+            else
+            {
+                Console.WriteLine("LOH");
+            }
+        }
+        private static void Test2()
+        {
+            byte[] test = { 0x3a, 0x96, 0xf9, 0xc7, 0xf6, 0x75, 0x5c, 0xfe, 0x46, 0xf0, 0x0e, 0x3d, 0xdc, 0xd5, 0xa4, 0x1e };
+            MainKey = new byte[] { 00, 01, 02, 03, 04, 05, 06, 07, 08, 09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f };
+            byte[] plaintText = { 00, 01, 02, 03, 04, 05, 06, 07, 08, 09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f };
+            string plaintTextstring = Encoding.ASCII.GetString(plaintText);
+            GenerateKeyTest(128, MainKey);
+            byte[] cipher = EncodeRc6Test(plaintTextstring);
+            if (test == cipher)
                 Console.WriteLine("gg easy");
             else
             {
@@ -195,7 +323,7 @@ namespace RC6 // W=32  R= 20 B = 128,192,256
             {
                 try
                 {
-                    GenerateKey(Long,null);
+                    GenerateKeyTest(Long,null);
                 }
                 catch (Exception e)
                 {
@@ -219,7 +347,7 @@ namespace RC6 // W=32  R= 20 B = 128,192,256
             plainText = Console.ReadLine();
             try
             {
-                cipherText = EncodeRc6(plainText);
+                cipherText = EncodeRc6Test(plainText);
             }
             catch (Exception e)
             {
