af7fc9b9b270f634aa9b1acff39904e83973eb19
Павел Петров <pashka499@yandex.ru>
2018-11-10 14:47:10 +0700

    Encrpition done
    Maybe it works incorrect

--- RC6/RC6/Program.cs
+++ RC6/RC6/Program.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.ComponentModel;
 using System.ComponentModel.Design;
+using System.Globalization;
 using System.Security.Cryptography;
 using System.Text;
 
-namespace RC6
+namespace RC6 // W=32  R= 20 B = 128,192,256
 {
     class Program
     {
         private const int R=20;
-        private static uint[] RoundKey = new uint[2*R+42];
+        private static uint[] RoundKey = new uint[2*R+4];
         private const int W = 32;
         private static byte[] MainKey;
         private const uint P32 = 0xB7E15163;
@@ -71,18 +72,62 @@ namespace RC6
                 j = (j + 1) % c;
             }
         }
+
+        private static byte[] ToArrayBytes(uint[] uints,int Long)
+        {
+            byte[] arrayBytes = new byte[Long*4];
+            for (int i = 0; i < Long ; i++)
+            {
+                byte[] temp = BitConverter.GetBytes(uints[i]);
+                temp.CopyTo(arrayBytes,i*4);
+            }
+            return arrayBytes;
+        }  
         private static byte[] EncodeRc6(string plaintext)
         {
-            byte[] kek = System.Text.Encoding.UTF8.GetBytes(plaintext);
-            return kek;
-        }
+            uint A, B, C, D; 
+            byte[] byteText = Encoding.UTF8.GetBytes(plaintext);
+            Console.WriteLine(byteText.Length);
+            int i = byteText.Length;    //
+            while (i % 16 != 0)         //
+                i++;                    //
+            byte [] text = new byte[i]; // 
+            byteText.CopyTo(text,0);    // мб можно проще и я аут, но это первое простое что пришло в голову, чтобы добавить 0 байты до размер блока 128 бит
+            byte[] cipherText = new byte[i];
+            for (i = 0; i < text.Length; i = i + 16)
+            {
+                A = BitConverter.ToUInt32(text,i);
+                B = BitConverter.ToUInt32(text, i + 4);
+                C = BitConverter.ToUInt32(text, i + 8);
+                D = BitConverter.ToUInt32(text, i + 12);
+                B = B + RoundKey[0];
+                D = D + RoundKey[1];
+                for (int j = 1; j <= R; j++)
+                {
+                    uint t = LeftShift((B * (2 * B + 1)), ShiftCount((int)(Math.Log(W,2))));
+                    uint u = LeftShift((D * (2 * D + 1)), ShiftCount((int) (Math.Log(W, 2))));
+                    A = (LeftShift((A ^ t), ShiftCount((int) u))) + RoundKey[j * 2];
+                    C = (LeftShift((C ^ u), ShiftCount((int)t))) + RoundKey[j * 2 + 1];
+                    uint temp = A;
+                    A = B;
+                    B = C;
+                    C = D;
+                    D = temp;
+                }
 
+                A = A + RoundKey[2 * R + 3];
+                C = C + RoundKey[2 * R + 3];
+                uint[] tempWords = new uint[4] {A,B,C,D};
+                byte[] block = ToArrayBytes(tempWords, 4);
+                block.CopyTo(cipherText,i);
+            }
+            return cipherText;
+        }
         private static string DecodeRc6(byte[] ciphertext)
         {
             string kek = Encoding.UTF8.GetString(ciphertext);
             return kek;
         }
-
         private static void Test()
         {
             byte[] test = {0x8f, 0xc3, 0xa5, 0x36, 0x56, 0xb1, 0xf7, 0x78, 0xc1, 0x29, 0xdf, 0x4e, 0x98, 0x48};
@@ -91,8 +136,7 @@ namespace RC6
             string plaintTextstring = Encoding.UTF8.GetString(plaintText);
             byte[] cipher = EncodeRc6(plaintTextstring);
             if (test==cipher)
-                Console.WriteLine("gg easy");
-            
+                Console.WriteLine("gg easy");           
         }
         private static void SelectKeySize()
         {
@@ -144,7 +188,12 @@ namespace RC6
                 Console.WriteLine(e);
                 throw;
             }
-            Console.WriteLine(cipherText);
+
+            //foreach (var bytes in cipherText)
+            //{
+            //    BitConverter.ToString()
+            //}
+            Console.WriteLine(Encoding.Default.GetString(cipherText));
             try
             {
                 plainText = DecodeRc6(cipherText);
